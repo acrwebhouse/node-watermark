1e6d3a8711229b29ddb9e8e93a77335263bac64c
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-09-18 16:30:38 +0800

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log(123)
+// var watermark = require('jimp-watermark');
+// var options = {
+//     'opacity': 0.6, //Should be less than one
+// 	'textSize': 1, //Should be between 1-8
+//     'dstPath' : './4.jpg'
+// };
+// watermark.addWatermark('/Users/chris/Documents/work/ACR/0917/1.jpeg', '/Users/chris/Documents/work/ACR/0917/Lovepik_com-450073212-watercolor painting flower leaf on transparent background.png',options).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
